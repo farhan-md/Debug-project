9524f7c7b8c476a4a5aa3467cf59669d93954edc
farhan-md <f.majidi1924@gmail.com>
2025-09-04 07:17:17 +0330

    Add student model with typo in field name

--- Debug-project/Debugproject/app/models.py
+++ Debug-project/Debugproject/app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    # Intentionally wrong: its name should be age
+    agee = models.IntegerField(default=18)
