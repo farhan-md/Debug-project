b56a8072627fa14627bdcc7c04d5f7dde9b8f458
farhan-md <f.majidi1924@gmail.com>
2025-09-04 09:07:37 +0330

    map missing_view in urls.py to resolve 404

--- Debug-project/Debugproject/app/urls.py
+++ Debug-project/Debugproject/app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.home_view , name="home")
-    # We intentionally don't put missing_page_view here → it gives 404
-    
+    path("",views.home_view , name="home"),
+    path("missing/",views.missing_view,name="missing"),
 ]
